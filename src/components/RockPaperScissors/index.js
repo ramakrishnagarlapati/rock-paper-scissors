374cfa00368e0f8412d64b08c7c4efa2aec1987b
ramakrishna <ramakrishnagarlapati04@gmail.com>
2023-11-01 14:04:10 +0000

    rock paper scissors game

--- src/components/RockPaperScissors/index.js
+++ src/components/RockPaperScissors/index.js
@@ -1,11 +1,10 @@
-import {useState, useEffect, useRef} from 'react'
+import {Component} from 'react'
 import Popup from 'reactjs-popup'
+import {RiCloseLine} from 'react-icons/ri'
 import {
   AppContainer,
   AppHeader,
-  OptionsList,
-  OptionItem,
-  OptionText,
+  AppHeading,
   ScoreBoard,
   ScoreBoardTitle,
   TotalScore,
@@ -27,46 +26,70 @@ import {
   GameStatusContainer,
   Status,
   PlayAgainBtn,
+  ResponsiveContainer,
 } from './styledComponents'
 
-function RockPaperScissors({choicesList}) {
-  const [totalScore, setTotalScore] = useState(0)
-  const [isPlaying, setIsPlaying] = useState(true)
-  const [userChoice, setUserChoice] = useState(null)
-  const [opponentChoice, setOpponentChoice] = useState(null)
-  const [gameStatus, setGameStatus] = useState(null)
-  const computeResult = () => {
+class RockPaperScissors extends Component {
+  state = {
+    totalScore: 0,
+    isPlaying: true,
+    userChoice: null,
+    opponentChoice: null,
+    gameStatus: null,
+  }
+
+  computeResult = () => {
+    const {userChoice, opponentChoice} = this.state
     if (userChoice === opponentChoice) {
-      setGameStatus('DRAW')
-      setIsPlaying(false)
+      this.setState({
+        gameStatus: 'DRAW',
+        isPlaying: false,
+      })
     } else if (
       (userChoice === 'PAPER' && opponentChoice === 'ROCK') ||
       (userChoice === 'SCISSORS' && opponentChoice === 'PAPER') ||
-      (userChoice === 'SCISSORS' && opponentChoice === 'PAPER')
+      (userChoice === 'ROCK' && opponentChoice === 'SCISSORS')
     ) {
-      setTotalScore(totalScore + 1)
-      setGameStatus('WIN')
-      setIsPlaying(false)
+      this.setState(prevState => ({
+        totalScore: prevState.totalScore + 1,
+        gameStatus: 'WON',
+        isPlaying: false,
+      }))
     } else if (
       (userChoice === 'PAPER' && opponentChoice === 'SCISSORS') ||
-      (userChoice === 'PAPER' && opponentChoice === 'ROCK') ||
+      (userChoice === 'ROCK' && opponentChoice === 'PAPER') ||
       (userChoice === 'SCISSORS' && opponentChoice === 'ROCK')
     ) {
-      setTotalScore(totalScore - 1)
-      setGameStatus('LOSE')
-      setIsPlaying(false)
+      this.setState(prevState => ({
+        totalScore: prevState.totalScore - 1,
+        gameStatus: 'LOSE',
+        isPlaying: false,
+      }))
     }
   }
-  useEffect(() => {
-    computeResult()
-  }, [userChoice])
-  const onClickControlButton = selectedChoiceId => {
-    setUserChoice(selectedChoiceId)
+
+  onClickControlButton = selectedChoiceId => {
+    const {choicesList} = this.props
     const randomNumber = Math.floor(Math.random() * 3)
-    setOpponentChoice(choicesList[randomNumber].id)
+    this.setState(
+      {
+        userChoice: selectedChoiceId,
+        opponentChoice: choicesList[randomNumber].id,
+      },
+      this.computeResult,
+    )
   }
 
-  const getTestId = id => {
+  onClickPlayAgainBtn = () => {
+    this.setState({
+      userChoice: null,
+      opponentChoice: null,
+      gameStatus: null,
+      isPlaying: true,
+    })
+  }
+
+  getTestId = id => {
     if (id === 'PAPER') {
       return 'paperButton'
     }
@@ -75,91 +98,111 @@ function RockPaperScissors({choicesList}) {
     }
     return 'scissorsButton'
   }
-  const renderGameControls = () => (
-    <ControlsList>
-      {choicesList.map(eachItem => (
-        <ControlItem key={eachItem.id}>
-          <ControlButton
-            type="button"
-            data-testid={getTestId(eachItem.id)}
-            onClick={() => onClickControlButton(eachItem.id)}
-          >
-            <ControlImage src={eachItem.imageUrl} alt={eachItem.id} />
-          </ControlButton>
-        </ControlItem>
-      ))}
-    </ControlsList>
-  )
-  const renderResultView = () => {
+
+  renderGameControls = () => {
+    const {choicesList} = this.props
+    return (
+      <ControlsList>
+        {choicesList.map(eachItem => (
+          <ControlItem key={eachItem.id}>
+            <ControlButton
+              type="button"
+              data-testid={this.getTestId(eachItem.id)}
+              onClick={() => this.onClickControlButton(eachItem.id)}
+            >
+              <ControlImage src={eachItem.imageUrl} alt={eachItem.id} />
+            </ControlButton>
+          </ControlItem>
+        ))}
+      </ControlsList>
+    )
+  }
+
+  renderResultView = () => {
+    const {userChoice, opponentChoice, gameStatus} = this.state
+    const {choicesList} = this.props
     const getImageUrl = id => {
       const object = choicesList.find(item => item.id === id)
       return object.imageUrl
     }
     return (
-      userChoice && (
-        <ResultViewContainer>
-          <Players>
-            <Player>
-              <PlayerLabel>You</PlayerLabel>
-              <PlayerChoiceImage
-                src={getImageUrl(userChoice)}
-                alt="your choice"
-              />
-            </Player>
-            <Player>
-              <PlayerLabel>Opponent</PlayerLabel>
-              <PlayerChoiceImage
-                src={getImageUrl(opponentChoice)}
-                alt="opponent choice"
-              />
-            </Player>
-          </Players>
+      <ResultViewContainer>
+        <Players>
+          <Player>
+            <PlayerLabel>You</PlayerLabel>
+            <PlayerChoiceImage
+              src={getImageUrl(userChoice)}
+              alt="your choice"
+            />
+          </Player>
+          <Player>
+            <PlayerLabel>Opponent</PlayerLabel>
+            <PlayerChoiceImage
+              src={getImageUrl(opponentChoice)}
+              alt="opponent choice"
+            />
+          </Player>
+        </Players>
 
-          <GameStatusContainer>
-            <Status>
-              {gameStatus === 'DRAW' ? 'IT IS DRAW' : `YOU ${gameStatus}`}
-            </Status>
-            <PlayAgainBtn type="button">Play Again</PlayAgainBtn>
-          </GameStatusContainer>
-        </ResultViewContainer>
-      )
+        <GameStatusContainer>
+          <Status>
+            {gameStatus === 'DRAW' ? 'IT IS DRAW' : `YOU ${gameStatus}`}
+          </Status>
+          <PlayAgainBtn type="button" onClick={this.onClickPlayAgainBtn}>
+            PLAY AGAIN
+          </PlayAgainBtn>
+        </GameStatusContainer>
+      </ResultViewContainer>
     )
   }
-  return (
-    <AppContainer>
-      <AppHeader>
-        <OptionsList>
-          {choicesList.map(eachChoice => (
-            <OptionItem key={eachChoice.id}>
-              <OptionText>{eachChoice.id}</OptionText>
-            </OptionItem>
-          ))}
-        </OptionsList>
-        <ScoreBoard>
-          <ScoreBoardTitle>Score</ScoreBoardTitle>
-          <TotalScore>{totalScore}</TotalScore>
-        </ScoreBoard>
-      </AppHeader>
-      <GameContainer>
-        {isPlaying ? renderGameControls() : renderResultView()}
-      </GameContainer>
-      <PopupContainer>
-        <Popup modal trigger={<RulesButton type="button">Rules</RulesButton>}>
-          {close => (
-            <ModalContainer>
-              <CloseButton type="button" onClick={() => close()}>
-                Close
-              </CloseButton>
 
-              <RulesImage
-                src="https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rules-image.png"
-                alt="rules"
-              />
-            </ModalContainer>
-          )}
-        </Popup>
-      </PopupContainer>
-    </AppContainer>
-  )
+  render() {
+    const {choicesList} = this.props
+    const {totalScore, isPlaying} = this.state
+    return (
+      <AppContainer>
+        <ResponsiveContainer>
+          <AppHeader>
+            <AppHeading>
+              ROCK
+              <br />
+              PAPER
+              <br />
+              SCISSORS
+              <br />
+            </AppHeading>
+
+            <ScoreBoard>
+              <ScoreBoardTitle>Score</ScoreBoardTitle>
+              <TotalScore>{totalScore}</TotalScore>
+            </ScoreBoard>
+          </AppHeader>
+          <GameContainer>
+            {isPlaying ? this.renderGameControls() : this.renderResultView()}
+          </GameContainer>
+          <PopupContainer>
+            <Popup
+              modal
+              trigger={<RulesButton type="button">Rules</RulesButton>}
+              overlayStyle={{backgroundColor: 'rgba(0,0,0,0.6)'}}
+            >
+              {close => (
+                <ModalContainer>
+                  <CloseButton type="button" onClick={() => close()}>
+                    <RiCloseLine size={20} color="#231f20" />
+                  </CloseButton>
+
+                  <RulesImage
+                    src="https://assets.ccbp.in/frontend/react-js/rock-paper-scissor/rules-image.png"
+                    alt="rules"
+                  />
+                </ModalContainer>
+              )}
+            </Popup>
+          </PopupContainer>
+        </ResponsiveContainer>
+      </AppContainer>
+    )
+  }
 }
 export default RockPaperScissors
--- src/components/RockPaperScissors/styledComponents.js
+++ src/components/RockPaperScissors/styledComponents.js
@@ -1,59 +1,196 @@
 import styled from 'styled-components'
 
-export const AppContainer = styled.div``
-
-export const AppHeader = styled.div``
-
-export const OptionsList = styled.ul``
-
-export const OptionItem = styled.li``
-
-export const OptionText = styled.p``
-
-export const ScoreBoard = styled.div``
-
-export const ScoreBoardTitle = styled.p``
-
-export const TotalScore = styled.p``
-
-export const GameContainer = styled.div``
-
-export const PopupContainer = styled.div``
-
-export const RulesButton = styled.button``
-
-export const RulesImage = styled.img`
-  width: 300px;
+export const AppContainer = styled.div`
+  font-family: 'Bree Serif';
+  min-height: 100vh;
+  background-color: #223a5f;
+  color: #fff;
+  display: flex;
+  justify-content: center;
+`
+export const ResponsiveContainer = styled.div`
+  width: 80%;
+  margin: 20px auto;
+  display: flex;
+  flex-direction: column;
+  @media (min-width: 768px) {
+    margin: 50px auto;
+  }
 `
 
-export const ModalContainer = styled.div``
+export const AppHeader = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 20px;
+  border: 1px solid #fff;
+  border-radius: 8px;
+  margin: 0 auto;
+  width: 100%;
+  @media (min-width: 768px) {
+    max-width: 800px;
+  }
+`
 
-export const CloseButton = styled.button``
+export const AppHeading = styled.h1`
+  font-size: 18px;
+  font-weight: 500;
+`
 
-export const ControlsList = styled.ul``
+export const ScoreBoard = styled.div`
+  background-color: #fff;
+  border-radius: 8px;
+  padding: 20px;
+`
+
+export const ScoreBoardTitle = styled.p`
+  color: #223a5f;
+  font-size: 18px;
+  font-weight: 500;
+`
+
+export const TotalScore = styled.p`
+  font-family: 'Roboto';
+  text-align: center;
+  font-weight: 700;
+  font-size: 26px;
+  color: #223a5f;
+`
+
+export const GameContainer = styled.div`
+  flex-grow: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`
+
+export const PopupContainer = styled.div`
+  align-self: flex-end;
+`
+
+export const RulesButton = styled.button`
+  background-color: #fff;
+  color: #223a5f;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  border-radius: 5px;
+  font-family: inherit;
+  font-size: 16px;
+  padding: 8px 16px;
+`
+
+export const RulesImage = styled.img`
+  width: 100%;
+  height: 80%;
+`
+
+export const ModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  background-color: #fff;
+  padding: 20px;
+  gap: 10px;
+  @media (min-width: 768px) {
+    width: 550px;
+    height: 450px;
+  }
+`
+
+export const CloseButton = styled.button`
+  border: none;
+  align-self: flex-end;
+  padding: 8px;
+  cursor: pointer;
+`
+
+export const ControlsList = styled.ul`
+  list-style-type: none;
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+
+  @media (min-width: 768px) {
+    width: 400px;
+  }
+`
 
 export const ControlItem = styled.li``
 
-export const ControlButton = styled.button``
+export const ControlButton = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+`
 
 export const ControlImage = styled.img`
   width: 150px;
 `
 
-export const ResultViewContainer = styled.div``
+export const ResultViewContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 20px;
+  @media (min-width: 768px) {
+    gap: 50px;
+  }
+`
 
-export const Players = styled.div``
+export const Players = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 20px;
 
-export const Player = styled.div``
+  @media (min-width: 768px) {
+    gap: 100px;
+  }
+`
 
-export const PlayerLabel = styled.p``
+export const Player = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+
+  @media (min-width: 768px) {
+    gap: 25px;
+  }
+`
+
+export const PlayerLabel = styled.p`
+  font-family: 'Roboto';
+  font-size: 20px;
+  font-weight: 500;
+`
 
 export const PlayerChoiceImage = styled.img`
   width: 150px;
 `
 
-export const GameStatusContainer = styled.div``
+export const GameStatusContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+`
 
-export const Status = styled.p``
+export const Status = styled.p`
+  font-family: 'Roboto';
+  font-size: 24px;
+  font-weight: 600;
+`
 
-export const PlayAgainBtn = styled.button``
+export const PlayAgainBtn = styled.button`
+  font-family: inherit;
+  font-size: 16px;
+  font-weight: 600;
+  background-color: #fff;
+  color: #223a5f;
+  border: none;
+  cursor: pointer;
+  outline: none;
+  border-radius: 5px;
+  padding: 12px 35px;
+`
